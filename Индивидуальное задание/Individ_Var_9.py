734851f1f8dda805fd07655fe4c6d8cf47f4a678
2302Nikolay <Malzewn@gmail.com>
2022-06-02 14:02:39 +0300

    Добавил enumerate

--- Индивидуальное задание/Individ_Var_9.py
+++ Индивидуальное задание/Individ_Var_9.py
@@ -4,14 +4,14 @@
 if __name__ == '__main__':
 
     # Картеж с числами для теста работы программы:
-    # A = (9, 2, 5, 3, 6, 10, 5, 6, 0, 13, 2)
-    A = tuple(map(int, input().split()))
+    A = (9, 2, 5, 3, 6, 10, 5, 6, 0, 13, 2)
+    # A = tuple(map(int, input().split()))
 
     ind = 0
     # Нахожу последнюю тройку элементов, в которой средний больше соседних
-    for i in range(1, len(A)):
-        if A[i - 1] < A[i] and A[i + 1] < A[i]:
-            ind = i
+    for n, i in enumerate(A):
+        if A[n - 1] < A[n] and A[n + 1] < A[n]:
+            ind = n
 
     # Вывожу в строку все элементы, предшествующие этой тройки
     print([A[i] for i in range(0, ind-1)])
